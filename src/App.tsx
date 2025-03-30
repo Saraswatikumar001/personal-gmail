cdd0b20ead9e9f3fce2dce155d1b8dce315e6cf7
Jeet <jeetnath2110@gmail.com>
2025-03-30 22:27:30 +0530

    Jit

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,10 @@ function App() {
 
   return (
     <>
-    <Navbar />
-      <h1 className='font-bold text-[8vh] capitalize'>briliant software developer <span className='font-[900] uppercase'>{Name}</span></h1>
+      <Navbar />
+      <h1 className='font-[900] text-[8vh] uppercase'>{Name}</h1>
+      <h3 className='text-xl capitalize font-[500]'>A briliant software developer</h3>
+      <button className='capitalize text-[2vh] mt-5 bg-sky-500 px-4 py-2 rounded-[50px] text-white hover:bg-sky-800 transition delay-150 duration-200 ease-in-out'>click for more info</button>
     </>
   )
 }
