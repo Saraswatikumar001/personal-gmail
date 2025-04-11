41ef4ca6b122e83d4d0f2a543a6d812184ed0efe
Jeet <jeetnath2110@gmail.com>
2025-04-11 23:16:38 +0530

    Jit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import './App.css'
 import Mainnav from './primary-components/Main-nav'
+import Secondnavbar from './primary-components/Second-nav'
 
 function App() {
 
 
   return (
     <>
-      <div className='grid grid-cols-2'>
-        <div>
+      <div className="flex flex-col md:flex-row">
+        <div className="bg-blue-500 p-4 flex text-white">
           <Mainnav />
         </div>
+        <div className="bg-blue-500 p-4 flex-3 text-white">
+          <Secondnavbar />
+        </div>
+        <div className="bg-green-500 p-4 flex-15 text-white">
 
+        </div>
       </div>
     </>
   )
---
+++ src/primary-components/Second-nav.tsx
@@ -0,0 +1,9 @@
+const Secondnavbar = () => {
+    return (
+        <>
+        hello
+        </>
+    )
+}
+
+export default Secondnavbar;
