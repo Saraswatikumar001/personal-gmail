5218630d11819827224e8409a139d172672a9b8d
Jeet <jeetnath2110@gmail.com>
2025-04-24 07:26:33 +0530

    Main nav customize continue

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,14 @@ function App() {
 
   return (
     <>
-      <div className='flex flex-col md:flex-row'>
-        <div className='bg-blue-500 p-4 flex-1 text-white'>
+      <div className='flex flex-col md:flex-row h-[100vh]'>
+        <div className='p-4 flex-1 text-white h-full'>
           <Mainnav />
         </div>
-        <div className='bg-green-500 p-4 flex-2 text-white'>
+        <div className='p-4 flex-2 text-white'>
           <Secondnavbar />
         </div>
-        <div className='bg-red-500 p-4 flex-15 text-white'>
+        <div className='p-4 flex-15 text-white'>
           <Secondnavbar />
         </div>
       </div>
