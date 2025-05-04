c42d38e90a6669c3442447e5b0daa4c015086527
Shruti <kusaraswati45@gmail.com>
2025-05-04 18:54:52 +0530

    sa

--- src/primary-components/Second-nav.tsx
+++ src/primary-components/Second-nav.tsx
@@ -4,8 +4,11 @@ const Secondnavbar = () => {
             <div className="max-w-sm h-full pt-5">
                 <div className="flex items-center mt-1 justify-center">
                     <i className=" text-3xl bi bi-envelope-at  text-black"></i>
-                    <p className="text-3xl text-black capitalize ms-3 font-normal">gmail</p>
+                    <p className="text-3xl text-black capitalize ms-3 font-normal" style={{ fontFamily: "sans-serif" }}>gmail</p>
                 </div>
+                <button className=" text-white font-bold py-2 px-4 mt-12 rounded cursor-pointer">
+                    <i className="fa-solid fa-wand-magic text-black text-2xl"></i>
+                </button>
             </div>
         </>
     )
