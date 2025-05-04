8e210537d7e828996273b4244fd4d97038c5294b
Shruti <kusaraswati45@gmail.com>
2025-05-04 18:30:49 +0530

    saraswati

--- src/primary-components/Second-nav.tsx
+++ src/primary-components/Second-nav.tsx
@@ -2,7 +2,7 @@ const Secondnavbar = () => {
     return (
         <>
             <div className="max-w-sm h-full pt-5">
-                <div className="flex items-center mt-3 justify-center">
+                <div className="flex items-center mt-1 justify-center">
                     <i className=" text-3xl bi bi-envelope-at  text-black"></i>
                     <p className="text-3xl text-black capitalize ms-3">gmail</p>
                 </div>
