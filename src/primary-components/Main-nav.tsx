95ba9517227f60d9c5d312b75d14a69c433f86ce
Shruti <kusaraswati45@gmail.com>
2025-05-04 16:15:23 +0530

    saraswati

--- src/primary-components/Main-nav.tsx
+++ src/primary-components/Main-nav.tsx
@@ -1,16 +1,7 @@
 const Navbar = () => {
     return (
         <>
-            <div className="max-w-sm rounded overflow-hidden shadow-lg h-full flex justify-center py-5">
-                <div>
-                    <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded cursor-pointer">
-                        <i className="fa-solid fa-bars text-black text-2xl"></i>
-                    </button>
-                    <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded cursor-pointer">
-                        <i className="fa-solid fa-bars text-black text-2xl"></i>
-                    </button>
-                </div>
-            </div>
+        fhhfkdk
         </>
     )
 }
