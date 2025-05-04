638a6a1306786e3f46dae55f5af84baa641e0840
Jeet <jeetnath2110@gmail.com>
2025-05-04 19:24:20 +0530

    Git push

--- src/primary-components/Second-nav.tsx
+++ src/primary-components/Second-nav.tsx
@@ -6,9 +6,9 @@ const Secondnavbar = () => {
                     <i className=" text-3xl bi bi-envelope-at  text-black"></i>
                     <p className="text-3xl text-black capitalize ms-3 font-normal" style={{ fontFamily: "sans-serif" }}>gmail</p>
                 </div>
-               <button className="flex text-white font-bold py-2 w-[80%] ms-[15%] px-4 mt-12 rounded cursor-pointer bg-[#e9eef6]">
-                    <i className="fa-solid fa-wand-magic text-black text-sm"></i>
-                    <p className="text-sm text-black capitalize ms-3 font-normal" style={{ fontFamily: "sans-serif" }}>compose</p>
+               <button className="flex text-white font-bold p-3 shadow-md w-[90%] ms-[10%] rounded-full mt-12 rounded cursor-pointer bg-[#e9eef6] items-center">
+                    <i className="fa-solid fa-wand-magic text-black text-[15px]"></i>
+                    <p className=" text-[18px] text-black capitalize ms-3" style={{ fontFamily: "sans-serif" }}>compose</p>
                 </button>               
             </div>
         </>
